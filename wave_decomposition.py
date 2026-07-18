8702901c366f815fa7e02dd279f398373be010bb
LuizFillip <luizfillip@outlook.com>
2026-07-18 07:22:48 -0300

    compute JAWARA decomposition

---
+++ wave_decomposition.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 18 07:22:07 2026
+
+@author: Luiz
+"""
+
+
+import SSW25PW as p
+import waves as wv
+
+
+period_min, period_max = 2.1, 10
+
+
+
+# fig, ax = plt.subplots(
+#     dpi = 300,
+#     nrows = 2, 
+#     figsize = (12, 10))
+
+
+    
+
+
+start, end = 60, 90
+# mat = mat.loc[:, (mat.columns >= start) &  (mat.columns <= end ) ].copy()
+
+# filtered = b.pass_band_2d(
+#     mat,
+#     bandpass = (period_min, period_max)
+# )
+
+# vmax = np.nanpercentile( np.abs(filtered), 98 )
+
+ 
+# # levels = np.linspace( -vmax, vmax, 30)
+# img = ax[0].contourf(
+#     filtered.columns, 
+#     filtered.index, 
+#     filtered, 
+#     levels = 30, 
+#     cmap = 'seismic'
+#     )
+ 
+# plt.colorbar(img)
+
+
+# signal = b.bandpass_1d(
+#     signal,
+#     low_period= period_min,
+#     high_period= period_max,
+# )
+
+
+# # result = p.plot_Wavelet(
+# #     ax[1],
+# #     time,
+# #     filtered,
+# #     period_min = period_min,
+# #     period_max = period_max,
+    
+#     # )
+# # wv.plot_zonalnumber_decomposition(
+# #         ax[1],  
+# #         filtered,
+# #         period_min = period_min, 
+# #         period_max = period_max + 10,
+# #         y = 0.85,
+# #         x = 0.05,
+# #         fontsize = 30,
+# #         colorbar = True
+# #         )
+
+# # ax[1].set(xlim = [-5, 5])
+
