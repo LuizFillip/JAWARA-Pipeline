87916d5ada01d541188f1d18166086e764a89006
LuizFillip <luizfillip@outlook.com>
2026-07-21 08:03:47 -0300

    howloller digram for jawara

--- plot_latitude_heat_zonal.py
+++ plot_latitude_heat_zonal.py
@@ -1,58 +1,59 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.colors import TwoSlopeNorm
+import numpy as np 
+ 
+
+def join_data():
+    out = []
+    for i in range(1, 5):
+        infile = f'JAWARA/data/lat_7/T250{i}.txt'
+         
+        df = pd.read_csv(infile, index_col=0)
+         
+        ds = pd.pivot_table(
+            df, 
+            values = 't',
+            index = 'altitude', 
+            columns = 'time')
+        out.append(ds)
+        
+    return pd.concat(out, axis = 1)
 
 
-def plot_heigth_latitude(F_plot):
-    import matplotlib.pyplot as plt
-    from matplotlib.colors import TwoSlopeNorm
-    
-    fig, ax = plt.subplots(
-        figsize=(10, 6)
-    )
-    
-    vmax = float(
-        np.nanpercentile(
-            np.abs(F_plot.values),
-            90
-        )
-    )
-    
-    norm = TwoSlopeNorm(
-        vmin=-vmax,
-        vcenter=0,
-        vmax=vmax
-    )
-    F_plot = F_plot.where(
-        F_plot["altitude"] > 20,
-        drop=True
-    )
-    
-    contour = ax.contourf(
-        F_plot["latitude"],
-        F_plot["altitude"],
-        F_plot,
-        levels=61,
-        cmap="RdBu_r",
-        # norm=norm,
-        # extend="both"
-    )
-    
-    cbar = fig.colorbar(
-        contour,
-        ax=ax,
-        pad=0.02
-    )
-    
-    cbar.set_label(
-        r"$F_{\phi}$ (kg s$^{-2}$)"
-    )
-    
-    ax.set_xlabel("Latitude (°)")
-    ax.set_ylabel("Altitude (km)")
-    ax.set_title(
-        r"Horizontal EP flux $F_{\phi}$ — January 2025"
-    )
-    
-    # ax.set_ylim(20, 110)
-    # ax.set_xlim(-90, 90)
-    
-    plt.tight_layout()
-    plt.show()
+ds = join_data()
+fig, ax = plt.subplots(
+     figsize = (10, 6),
+     dpi = 300
+ )
+ 
+ 
+ 
+ds.columns = pd.to_datetime(ds.columns)
+
+ds = ds.loc[(ds.index > 20) & (ds.index < 120)]
+contour = ax.contourf(
+    ds.columns,
+    ds.index,
+    ds.values,
+    levels=61,
+    cmap="turbo",
+    # norm=norm,
+    # extend="both"
+)
+
+cbar = fig.colorbar(
+    contour,
+    ax=ax,
+    pad=0.02
+)
+
+cbar.set_label(
+    r"$F_{\phi}$ (kg s$^{-2}$)"
+)
+
+ax.set_xlabel("Latitude (°)")
+ax.set_ylabel("Altitude (km)")
+ 
+ax.set_ylim(20, 120)
+# ax.set_xlim(-90, 90)
