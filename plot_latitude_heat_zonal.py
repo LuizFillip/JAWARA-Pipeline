1e5e99b0900768e37e3a8830f40a523ed2fea820
LuizFillip <luizfillip@outlook.com>
2026-08-03 21:48:16 -0300

    updates

--- plot_latitude_heat_zonal.py
+++ plot_latitude_heat_zonal.py
@@ -20,40 +20,40 @@ def join_data():
         
     return pd.concat(out, axis = 1)
 
-
-ds = join_data()
-fig, ax = plt.subplots(
-     figsize = (10, 6),
-     dpi = 300
- )
- 
- 
- 
-ds.columns = pd.to_datetime(ds.columns)
-
-ds = ds.loc[(ds.index > 20) & (ds.index < 120)]
-contour = ax.contourf(
-    ds.columns,
-    ds.index,
-    ds.values,
-    levels=61,
-    cmap="turbo",
-    # norm=norm,
-    # extend="both"
-)
-
-cbar = fig.colorbar(
-    contour,
-    ax=ax,
-    pad=0.02
-)
-
-cbar.set_label(
-    r"$F_{\phi}$ (kg s$^{-2}$)"
-)
-
-ax.set_xlabel("Latitude (°)")
-ax.set_ylabel("Altitude (km)")
- 
-ax.set_ylim(20, 120)
-# ax.set_xlim(-90, 90)
+def main():
+    ds = join_data()
+    fig, ax = plt.subplots(
+         figsize = (10, 6),
+         dpi = 300
+     )
+     
+     
+     
+    ds.columns = pd.to_datetime(ds.columns)
+    
+    ds = ds.loc[(ds.index > 20) & (ds.index < 120)]
+    contour = ax.contourf(
+        ds.columns,
+        ds.index,
+        ds.values,
+        levels=61,
+        cmap="turbo",
+        # norm=norm,
+        # extend="both"
+    )
+    
+    cbar = fig.colorbar(
+        contour,
+        ax=ax,
+        pad=0.02
+    )
+    
+    cbar.set_label(
+        r"$F_{\phi}$ (kg s$^{-2}$)"
+    )
+    
+    ax.set_xlabel("Latitude (°)")
+    ax.set_ylabel("Altitude (km)")
+     
+    ax.set_ylim(20, 120)
+    # ax.set_xlim(-90, 90)
